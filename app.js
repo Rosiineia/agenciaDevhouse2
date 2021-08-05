5d180859410b0732a4954b4fed6a14eae012e082
Rosineia dos Santos <neyasantoss@gmail.com>
2021-08-04 23:03:30 -0300

    ejs

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 //IMPORTANDO MODULOS
 
 const express = require("express");
+const path = require("path");
+const rotasDeProdutos = require("./routes/produtos");
+const rotas = require("./routes");
 
 const app = express();
-
-const rotasDeProdutos= require('./routes/produtos');
-const rotas = require('./routes');
+// configurando o template engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve("views"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(rotasDeProdutos);
+app.use(rotas);
 
-app.listen(3000, ()=> console.log("o servidor foi instalado"));
+app.listen(3000);
 
+// tem criar o arquivo home em HTML que contenha um titulo e um paragrafo
+// esse arquivo vai ser exibido tanto na rota / quanto na rota /home
+//nodemon starting app.js
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,36 @@
-const express= require("express");
+const express = require("express");
+const path = require("path");
+const routes = express.Router();
 
-const routes= express.Router();
+const depoimentos = require("../models/depoimentos");
 
-module.express= routes;
+/*routes.get("/", (req, res) => {
+  const usuario = undefined;
+  res.render("home", { titulo: "Sua empresa vai ser incrivel!", usuario });
+});*/
 
-routes.use(express.static('public'))//COM ESSA EXTENSÃO BASTA COLOCAR O NOME DA IMAGEM
+routes.get("/home", (req, res) => {
+  res.redirect("/");
+}); 
 
-routes.get("/", (req, res) => {
-    res.sendFile(__dirname +"/views/home.html")
-})
+routes.get("/manutencao", (req, res) => {
+  res.sendFile(path.resolve("views", "manutencao.html"));
+});
 
+routes.get("/blog", (req, res) => {
+  res.sendFile(path.resolve("views", "blog.html"));
+});
 
+routes.get("/contato", (req, res) => {
+  res.sendFile(path.resolve("views", "contato.html"));
+});
 
+routes.post("/receber-contato", (req, res) => {
+  res.send("Contato recebido por:" + req.body.nome);
+});
 
-routes.get("/", (req, res)=>{
-    res.redirect("/");
-})
-//ROTAS
+routes.get("/depoimentos", (req, res) => {
+  res.render("depoimentos", { depoimentos, titulo: "Depoimentos" });
+});
 
-//routes.get("/", (req, res)=>res.send("Olá, estamos na página home"))
-
-/*routes.get("/", (req, res)=> { res.send("seja bem vindo")
-})*/
-
-//MOSTRANDO ERROS 
-/*routes.get("/manutencao",(req, res) =>{ res.send( "Site em manutenção")
-})*/
-
-//colocando imagens - encntrar um caminho absoluto
-
-routes.get("/home",(req, res)=>{res.sendFile(__dirname + "/views/home.html")}); /*usamos em todas as paginas 
-para buscar as pastas e aparecer no servidor*/
-routes.get("/blog",(req, res)=>{res.sendFile(__dirname + "/views/blog.html")});
-routes.get("/blog",(req, res)=>{res.sendFile(__dirname + "../public/img/destaque.webp")});
-
-routes.get("contato",(req, res)=>{res.sendFile(__dirname + "/views/contato.html")});
-
-
-//routes.get("/manutencao",(req, res)=>{res.sendFile(__dirname + "/views/manutencao.html")});
+module.exports = routes;
